98afe3bb298b28291eb125fac84b2f8cf85a612f
Sriyan-Nakka <sriyannakka@gmail.com>
2025-08-28 21:02:28 +0530

    version 0.5

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+import { calcSimpleInterest, simpleInterestAmt } from "./calc.js";
+
+const SIform = document.querySelector("#simpleInterestForm");
+
 function calculateInterest(e) {
   e.preventDefault();
   document.querySelector("#amountPara1").style.display = "block";
+
+  let principalAmount = document.querySelector("#principalAmount").value;
+  let years = document.querySelector("#years").value;
+  let interestRate = document.querySelector("#interestRate").value;
+
+  let amount = calcSimpleInterest(principalAmount, years, interestRate);
+  let interest = simpleInterestAmt(amount, principalAmount);
+
+  document.querySelector("#amountSpan1").textContent = amount;
+  document.querySelector("#interestSpan1").textContent = interest;
 }
+
+SIform.addEventListener("submit", calculateInterest);
