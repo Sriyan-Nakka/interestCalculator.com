7b325238d0289797a4f8d6bfea90136f92c9aa89
Sriyan-Nakka <sriyannakka@gmail.com>
2025-08-30 22:15:54 +0530

    version 0.9

--- calc.js
+++
@@ -1,7 +0,0 @@
-export function calcSimpleInterest(p, t, r) {
-  return p * (1 + (t * r) / 100);
-}
-
-export function simpleInterestAmt(a, p) {
-  return a - p;
-}
--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-import { calcSimpleInterest, simpleInterestAmt } from "./calc.js";
+import {
+  calcSimpleInterest,
+  calcCompoundInterest,
+  interestAmt,
+} from "../calc.js";
 
 const SIform = document.querySelector("#simpleInterestForm");
+const CIform = document.querySelector("#compoundInterestForm");
 
-function calculateInterest(e) {
+function calculateSimpleInterest(e) {
   e.preventDefault();
-  document.querySelector("#amountPara1").style.display = "block";
+  document.querySelector("#interestParaSI").style.display = "block";
+  document.querySelector("#amountParaSI").style.display = "block";
 
-  let principalAmount = document.querySelector("#principalAmount").value;
-  let years = document.querySelector("#years").value;
-  let interestRate = document.querySelector("#interestRate").value;
+  let principalAmount = document.querySelector("#SIprincipalAmount").value;
+  let years = document.querySelector("#SIyears").value;
+  let interestRate = document.querySelector("#SIinterestRate").value;
 
   let amount = calcSimpleInterest(principalAmount, years, interestRate);
-  let interest = simpleInterestAmt(amount, principalAmount);
+  let interest = interestAmt(amount, principalAmount);
 
-  document.querySelector("#amountSpan1").textContent = amount;
-  document.querySelector("#interestSpan1").textContent = interest;
+  document.querySelector("#amountSpanSI").textContent = amount;
+  document.querySelector("#interestSpanSI").textContent = interest;
 }
 
-SIform.addEventListener("submit", calculateInterest);
+SIform.addEventListener("submit", calculateSimpleInterest);
+
+function calculateCompoundInterest(e) {
+  e.preventDefault();
+  document.querySelector("#interestParaCI").style.display = "block";
+  document.querySelector("#amountParaCI").style.display = "block";
+
+  let principalAmount = document.querySelector("#CIprincipalAmount").value;
+  let interestRate = document.querySelector("#CIinterestRate").value;
+  let years = document.querySelector("#CIyears").value;
+
+  let amount = calcCompoundInterest(
+    principalAmount,
+    interestRate,
+    years
+  ).toFixed(3);
+  let interest = interestAmt(amount, principalAmount).toFixed(3);
+
+  document.querySelector("#amountSpanCI").textContent = amount;
+  document.querySelector("#interestSpanCI").textContent = interest;
+}
+
+CIform.addEventListener("submit", calculateCompoundInterest);
